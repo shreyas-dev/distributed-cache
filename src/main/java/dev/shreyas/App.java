ca69619c1c2d95c242fb6d6c62063664eb7e65d2
shreyas <shreyas@olaelectric.in>
2021-01-29 18:00:43 +0530

    first commit

---
+++ src/main/java/dev/shreyas/App.java
@@ -0,0 +1,13 @@
+package dev.shreyas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
